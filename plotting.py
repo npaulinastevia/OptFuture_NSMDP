05b3435aa2529689a6cdb556d91f9425bf2f66a7
npaulinastevia <npaulinastevia@gmail.com>
2024-01-31 10:59:27 -0500

    init

--- plotting.py
+++ plotting.py
@@ -4,8 +4,27 @@ import pandas as pd
 import numpy as np
 import csv
 #sns.barplot(data=df, x="data budgets", y="bug", hue="algorithms")
-li = pd.read_csv(r'C:\Users\panou\PycharmProjects\OptFuture_NSMDP\Src\results_cart1706042594.txt', index_col=None, header=0)
-sns.lineplot(data=li, y="x1", x="steps", hue="n_actions")
+li = pd.read_csv(r'C:\Users\panou\PycharmProjects\Sequoia\results_cart_testCRL1706181975.txt', index_col=None, header=0)
+
+#new=li.groupby('episodes')['reward'].sum() Cartpolev0  NSCartpolev0  NSCartpolev1  NSCartpolev2
+#new=pd.DataFrame(new)
+#print(li[li.Environments=='Cartpolev0'].groupby('episodes')['reward'].sum().to_list())
+x=li[li.Environments=='Cartpolev0'].groupby('episodes')['reward'].sum().to_list()[:92]
+x1=li[li.Environments=='NSCartpolev0'].groupby('episodes')['reward'].sum().to_list()[:92]
+x2=li[li.Environments=='NSCartpolev1'].groupby('episodes')['reward'].sum().to_list()[:92]
+x3=li[li.Environments=='NSCartpolev2'].groupby('episodes')['reward'].sum().to_list()[:92]
+
+a1=['Cartpolev0']*len(x)
+a2=['NSCartpolev0']*len(x)
+a3=['NSCartpolev1']*len(x)
+a4=['NSCartpolev2']*len(x)
+# 'episodes':[i for i in range(len(x))]+[i for i in range(len(x))] +[i for i in range(len(x))] +[i for i in range(len(x))]
+data={'Environments':a1+a2+a3+a4,
+      'episodes':[i for i in range(len(x))]+[i for i in range(len(x))] +[i for i in range(len(x))] +[i for i in range(len(x))],
+      'reward':x+x1+x2+x3}
+
+li=pd.DataFrame(data)
+sns.lineplot(data=li, y="reward", x="episodes", hue="Environments")
 # plt.legend()
 #plt.ylabel("Average cumulative reward", fontsize=15)
 #plt.xlabel("Steps", fontsize=15)
